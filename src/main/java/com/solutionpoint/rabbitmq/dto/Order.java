95fb27c1e3e0233a52cdceb89af47a2822039e43
nitishgupta978 <nitishgupta9785@gmail.com>
2022-06-26 13:29:25 +0530

    Added :sub package dto and order orderstatus class

---
+++ src/main/java/com/solutionpoint/rabbitmq/dto/Order.java
@@ -0,0 +1,17 @@
+package com.solutionpoint.rabbitmq.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Order {
+    private String orderId;
+    private String orderName;
+    private int quantity;
+    private double price;
+}
---
+++ src/main/java/com/solutionpoint/rabbitmq/dto/OrderStatus.java
@@ -0,0 +1,16 @@
+package com.solutionpoint.rabbitmq.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class OrderStatus {
+    private Order order;
+    private String orderStatus;
+    private String message;
+}
