a4393b53f4b4eaa08b460f97b8676d243377e199
nitishgupta978 <nitishgupta9785@gmail.com>
2022-06-26 13:24:58 +0530

    Addede : sub package and Message class

---
+++ src/test/java/com/solutionpoint/rabbitmq/RabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solutionpoint.rabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
